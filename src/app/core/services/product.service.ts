7cff5652f4fe6198ee71e2dd3b6c188562afac64
Igor-W <igori.stoian@gmail.com>
2026-01-13 16:47:06 +0200

    feat: enhance ProductService with additional methods for product management
    - Added methods to get product by ID, search products, and retrieve categories.
    - Implemented pagination and sorting functionality for product listings.
    - Included methods to add, update, and delete products.

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,14 +1,73 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import {
+  ICategory,
+  IProduct,
+  IProductResponse,
+  PaginatedResponse,
+} from '../../common/interfaces/IProduct';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private readonly http = inject(HttpClient);
-  readonly API_URL = 'https://dummyjson.com/products';
+  private readonly apiUrl = 'https://dummyjson.com/products';
 
-  getAll() {
-    return this.http.get<{ products: any[] }>(this.API_URL);
+  getAll(): Observable<IProductResponse> {
+    return this.http.get<IProductResponse>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.apiUrl}/${id}`);
+  }
+
+  search(query: string): Observable<IProductResponse> {
+    return this.http.get<IProductResponse>(`${this.apiUrl}/search?q=${query}`);
+  }
+
+  getPaginated<T = IProduct>(
+    limit: number,
+    skip: number,
+    select?: (keyof T)[]
+  ): Observable<PaginatedResponse<T>> {
+    let url = `${this.apiUrl}?limit=${limit}&skip=${skip}`;
+
+    if (select?.length) {
+      url += `&select=${select.join(',')}`;
+    }
+
+    return this.http.get<PaginatedResponse<T>>(url);
+  }
+
+  sortBy(sortBy: keyof IProduct, order: 'asc' | 'desc'): Observable<IProductResponse> {
+    return this.http.get<IProductResponse>(`${this.apiUrl}?sortBy=${sortBy}&order=${order}`);
+  }
+
+  getCategories(): Observable<ICategory[]> {
+    return this.http.get<ICategory[]>(`${this.apiUrl}/categories`);
+  }
+
+  getCategoryList(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/category-list`);
+  }
+
+  getProductsByCategory(category: string): Observable<IProductResponse> {
+    return this.http.get<IProductResponse>(`${this.apiUrl}/category/${category}`);
+  }
+
+  addProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(`${this.apiUrl}/add`, product);
+  }
+
+  updateProduct(id: number, product: Partial<IProduct>): Observable<IProduct> {
+    return this.http.put<IProduct>(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<IProduct & { isDeleted: boolean; deletedOn: string }> {
+    return this.http.delete<IProduct & { isDeleted: boolean; deletedOn: string }>(
+      `${this.apiUrl}/${id}`
+    );
   }
 }
